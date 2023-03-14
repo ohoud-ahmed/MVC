6205ad5f4b20c5e688f28ad123e213b1c1ebd9f0
Ohoud Ahmed <82897751+ohoud-ahmed@users.noreply.github.com>
2023-03-14 08:18:15 +0300

    Add files

---
+++ RcyMVC/Data/MVCDemoDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RcyMVC.Models.Domain;
+
+namespace RcyMVC.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options): base(options) {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ RcyMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RcyMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
